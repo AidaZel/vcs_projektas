8a55919d32b939ab6b7270523a2cf93c395ac38a
AidaZel <aida.zelvyte@gmail.com>
2020-01-08 17:54:50 +0200

    footeris, 2-3 puslapiai

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   $(".dropdown-trigger").dropdown();
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.parallax');
+//     var instances = M.Parallax.init(elems, options);
+//   });
+
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
   });
